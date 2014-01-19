f48a2f8f7e6cc68f96753ebdd755de2d8f60151b
guillaumesoul <guillaume.soullard@gmail.com>
2014-01-19 22:58:42 +0100

    init

---
+++ src/com/sdzee/servlets/Test.java
@@ -0,0 +1,45 @@
+package com.sdzee.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+    	String paramAuteur = request.getParameter( "auteur" );
+    	String message = "Transmission de variables : OK ! " + paramAuteur;
+    	request.setAttribute( "test", message );
+
+    	this.getServletContext().getRequestDispatcher( "/WEB-INF/test.jsp" ).forward( request, response );
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
